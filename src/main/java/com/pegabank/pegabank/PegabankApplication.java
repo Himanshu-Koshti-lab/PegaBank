dcb3069d3fa5a4aeb84155634b6e4d46a0c08b08
Himanshu Koshti <64060564+Himanshu-Koshti-lab@users.noreply.github.com>
2022-12-08 23:41:45 +0530

    Initial commit

---
+++ src/main/java/com/pegabank/pegabank/PegabankApplication.java
@@ -0,0 +1,13 @@
+package com.pegabank.pegabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegabankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PegabankApplication.class, args);
+	}
+
+}
