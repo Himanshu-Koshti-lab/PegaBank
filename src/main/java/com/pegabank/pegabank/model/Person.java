30e0cbf411cd00f54edcfee7182eb27623495d35
Himanshu Koshti <64060564+Himanshu-Koshti-lab@users.noreply.github.com>
2022-12-15 22:39:22 +0530

    add tag

--- src/main/java/com/pegabank/pegabank/model/Person.java
+++ src/main/java/com/pegabank/pegabank/model/Person.java
@@ -17,7 +17,6 @@ public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-
     private String name;
     private String email;
     private String password;
